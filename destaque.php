69a321e83f67d2ebb9774e2d64a35ed887930c99
unknown <LeoCavalcante07>
2018-09-20 13:30:03 -0300

    Layout pronto

---
+++ destaque.php
@@ -0,0 +1,215 @@
+<!doctype html>
+
+<html>
+    <head>
+        <title>Destaques</title>
+        <title>Destaques</title>
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+        
+        
+                <!-- Shared assets -->
+        <link rel="stylesheet" type="text/css" href="examples/_shared/css/style.css">
+
+        <!-- Example assets -->
+        <link rel="stylesheet" type="text/css" href="examples/connected-carousels/jcarousel.connected-carousels.css">
+
+        <script type="text/javascript" src="libs/jquery/jquery.js"></script>
+        <script type="text/javascript" src="dist/jquery.jcarousel.min.js"></script>
+
+        <script type="text/javascript" src="examples/connected-carousels/jcarousel.connected-carousels.js"></script>
+        
+        
+        
+        <meta charset="utf-8">
+    </head>
+    
+    <body>
+        <header>
+            <div id="caixa_header">
+                <div id="caixa_header_central">
+                    <div id="caixa_logo">
+                       
+                    </div>
+
+                    <div id="caixa_menu">
+                       <a href="index.php"> 
+                           <div class="caixa_optMenu">
+                            <p>Home</p>
+                           </div>
+                        </a>
+
+                        <a href="destaque.php">
+                            <div class="caixa_optMenu">
+                               <p>Destaque</p>
+                            </div>
+                        </a>
+
+                        <a href="sobre.php">
+                            <div class="caixa_optMenu">
+                                <p>Sobre</p>
+                            </div>
+                        </a>
+                        
+                        <a href="promocoes.php">
+                            <div class="caixa_optMenu">
+                                <p>Promoções</p> 
+                            </div>           
+                        </a>
+
+                        <a href="bancas.php">
+                            <div class="caixa_optMenu">
+                                <p>Bancas</p>
+                            </div> 
+                        </a>
+                        
+                        <a href="celebridade.php">
+                            <div class="caixa_optMenu">
+                                <p>Celebridade</p>
+                            </div> 
+                        </a>
+                        
+                        <a href="faleConosco.php">
+                            <div class="caixa_optMenu" >
+                                <p>Fale Conosco</p> 
+                            </div>           
+                        </a>
+                        
+                    </div>
+
+                    <div id="caixa_login">
+                        <div class="caixa_seg_login_label">
+                            <div class="caixa_login_label" style="margin-right: 30px;">
+                                <p>Usuário:</p>
+                            </div>
+
+                            <div class="caixa_login_label">
+                                <p>Senha:</p>
+                            </div>                          
+                        </div>
+                        
+                        <div class="caixa_seg_login_text">
+                            <div class="caixa_login_text">
+                                <input type="text" name="txtUsuario">                                
+                            </div>
+                            
+                            <div class="caixa_login_text">
+                                <input type="password" name="txtSenha">                           
+                            </div>                            
+                        </div>
+                        
+                        <div class="caixa_botao">
+                            <input type="submit" name="btnEntrar"  class="btnEntrar" value="Entrar">    
+                        </div>
+                      
+                    </div>   
+                    
+                </div>
+                
+                
+            </div>
+            
+            
+        </header>
+        
+        
+        
+     
+
+        
+        <div class="caixa_global">
+            <div id="caixa_especial"></div>
+            
+            
+            <div class="caixa_destaque_principal">
+                <div class="caixa_destaque_titulo">
+                    <h1>Destaques</h1>
+                </div>
+                
+                <div class="caixa_destaque_section1">
+                    <section>
+                        <div class="caixa_destaque_section1_titulo">
+                            <h2>Titulo</h2>
+                        </div>
+                        
+                        <div class="caixa_destaque_section1_texto">
+                            <p>Morena de olhos castanhos, cabelos negros como a noite e um corpo perfeito para seus vinte e poucos anos. Eu sabia que era apenas mais um em sua boca, que em pouco tempo seria outro e eu seria esquecido.</p>
+                        </div>
+                        
+                        <div class="caixa_destaque_section1_img">
+                        
+                        </div>
+                    </section>
+                </div>
+                
+                
+                <div class="caixa_destaque_section2">
+                    <section>
+                        <div class="caixa_destaque_section2_titulo">
+                            <h2>Titulo</h2>
+                        </div>
+                        
+                        <div class="caixa_destaque_section2_img">
+                            
+                        </div>
+                        
+                        <div class="caixa_destaque_section2_texto">
+                            <p>E o que ganhou com isso? Vai desmanchar junto com os carros de policia e de bandidos, as ambulâncias e caminhões de lixo, os populares e os que já foram de última geração, os velhos e os antigos novos.</p>
+                        </div>
+                    </section>
+                </div>   
+                
+                
+                <div class="caixa_destaque_section3">
+                    <section>
+                        <div class="caixa_destaque_section3_titulo">
+                            <h2>Titulo</h2>
+                        </div>
+                        
+                        <div class="caixa_destaque_section3_texto">
+                            <p>Morena de olhos castanhos, cabelos negros como a noite e um corpo perfeito para seus vinte e poucos anos. Eu sabia que era apenas mais um em sua boca, que em pouco tempo seria outro e eu seria esquecido.</p>
+                        </div>
+                        
+                        <div class="caixa_destaque_section3_img">
+                        
+                        </div>
+                    </section>
+                </div>                
+                
+            </div>
+            
+            <div class="caixa_anuncio">
+                <div class="anuncio">
+                    <img src="imagens/facebook.png" alt="facebook">
+                </div>
+                
+                <div class="anuncio">
+                    <img src="imagens/linkedin.png" alt="linkedin">
+                </div>
+                
+                <div class="anuncio">
+                    <img src="imagens/twitter.png" alt="twitter">
+                </div>                
+                
+                
+            </div> 
+            
+        </div>        
+        
+        <footer>
+            <div id="caixa_footer">
+                <div class="caixa_footer_central">
+                    <div class="caixa_footer_cima">
+                        <p>Copyrights© 2018. Todos os direitos reservados.</p> 
+                        
+                    </div>
+                    
+                    <div class="caixa_footer_baixo">
+                          <p>Bugs Bunny S/A. Avenidas das Rosas, 1789, 11° andar, São Paulo - SP.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        
+        <script src="js/slider.js"></script>
+    </body>
+</html>    
---
+++ sobre.php
@@ -0,0 +1,194 @@
+    <!doctype html>
+
+    <html>
+        <head>
+            <title>Sobre a empresa</title>
+
+            <link type="text/css" rel="stylesheet" href="css/style.css">
+
+
+                    <!-- Shared assets -->
+            <link rel="stylesheet" type="text/css" href="examples/_shared/css/style.css">
+
+            <!-- Example assets -->
+            <link rel="stylesheet" type="text/css" href="examples/connected-carousels/jcarousel.connected-carousels.css">
+
+            <script type="text/javascript" src="libs/jquery/jquery.js"></script>
+            <script type="text/javascript" src="dist/jquery.jcarousel.min.js"></script>
+
+            <script type="text/javascript" src="examples/connected-carousels/jcarousel.connected-carousels.js"></script>
+
+
+
+            <meta charset="utf-8">
+        </head>
+
+        <body>
+            <header>
+                <div id="caixa_header">
+                    <div id="caixa_header_central">
+                        <div id="caixa_logo">
+
+                        </div>
+
+                    <div id="caixa_menu">
+                       <a href="index.php"> 
+                           <div class="caixa_optMenu">
+                            <p>Home</p>
+                           </div>
+                        </a>
+
+                        <a href="destaque.php">
+                            <div class="caixa_optMenu">
+                               <p>Destaque</p>
+                            </div>
+                        </a>
+
+                        <a href="sobre.php">
+                            <div class="caixa_optMenu">
+                                <p>Sobre</p>
+                            </div>
+                        </a>
+                        
+                        <a href="promocoes.php">
+                            <div class="caixa_optMenu">
+                                <p>Promoções</p> 
+                            </div>           
+                        </a>
+
+                        <a href="bancas.php">
+                            <div class="caixa_optMenu">
+                                <p>Bancas</p>
+                            </div> 
+                        </a>
+                        
+                        <a href="celebridade.php">
+                            <div class="caixa_optMenu">
+                                <p>Celebridade</p>
+                            </div> 
+                        </a>
+                        
+                        <a href="faleConosco.php">
+                            <div class="caixa_optMenu" >
+                                <p>Fale Conosco</p> 
+                            </div>           
+                        </a>
+                        
+                    </div>
+
+                        <div id="caixa_login">
+                            <div class="caixa_seg_login_label">
+                                <div class="caixa_login_label" style="margin-right: 30px;">
+                                    <p>Usuário:</p>
+                                </div>
+
+                                <div class="caixa_login_label">
+                                    <p>Senha:</p>
+                                </div>                          
+                            </div>
+
+                            <div class="caixa_seg_login_text">
+                                <div class="caixa_login_text">
+                                    <input type="text" name="txtUsuario">                                
+                                </div>
+
+                                <div class="caixa_login_text">
+                                    <input type="password" name="txtSenha">                           
+                                </div>                            
+                            </div>
+
+                            <div class="caixa_botao">
+                                <input type="submit" name="btnEntrar"  class="btnEntrar"  value="Entrar">    
+                            </div>
+
+                        </div>   
+
+                    </div>
+
+
+                </div>
+
+
+            </header>
+
+
+
+
+
+
+            <div class="caixa_global">
+                <div id="caixa_especial"></div>
+
+                <section>
+                    <div class="caixa_principal_sobre">
+                        <div class="caixa_titulo_sobre">
+                            <h1>Sobre</h1>
+                        </div>
+                        <div class="caixa_sobre_section1">                    
+                            <div class="caixa_sobre_section1_img">
+
+                            </div>
+
+                            <div class="caixa_sobre_section1_texto">
+                                <p>A Bugs Bunny foi fundada no ano de 2003, por um </p>
+                                <p>grupo de estudantes</p>
+                                <p>A principio, a banca ficava na praça das </p>
+                                <p>aguas</p>
+
+                            </div>                    
+                        </div>
+
+                        <div class="caixa_sobre_section2">
+
+                            <div class="caixa_sobre_section2_text">
+                                <p>A principio, a revista abordava</p>
+                                <p>somente conteudos relacionados à Tecnologia da Informação</p>
+                            </div>
+
+                            <div class="caixa_sobre_section2_img">
+
+                            </div>                    
+                        </div>
+
+                    </div>            
+                </section>
+
+
+
+                <div class="caixa_anuncio">
+                    <div class="anuncio">
+                        <img src="imagens/facebook.png" alt="Facebook">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="imagens/linkedin.png" alt="Linkedin">
+                    </div>
+
+                    <div class="anuncio">
+                        <img src="imagens/twitter.png" alt= "Twitter">
+                    </div>                
+
+
+                </div> 
+
+
+            </div>        
+
+            <footer>
+                <div id="caixa_footer">
+                    <div class="caixa_footer_central">
+                        <div class="caixa_footer_cima">
+                            <p>Copyrights© 2018. Todos os direitos reservados.</p> 
+
+                        </div>
+
+                        <div class="caixa_footer_baixo">
+                              <p>Bugs Bunny S/A. Avenidas das Rosas, 1789, 11° andar, São Paulo - SP.</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+            <script src="js/slider.js"></script>
+        </body>
+    </html>
